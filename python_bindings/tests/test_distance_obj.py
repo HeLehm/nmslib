ee056bd4b791ebdb75f2f12a97019a5c23ac3f0d
HeLehm <hergen@hergen.eu>
2023-02-01 10:53:31 +0100

    extended dist test

--- python_bindings/tests/test_distance_obj.py
+++ python_bindings/tests/test_distance_obj.py
@@ -43,6 +43,24 @@ class TestOuterDistance(unittest.TestCase):
             for id, dist in zip(ids, dists):
                 self.assertAlmostEqual(string_index.getDistanceToObjRgt(i ,X[id]), dist, places=6)
         
+    def test_to_obj_mylingua(self):
+        index = nmslib.init(method='hnsw',space_params={
+            'wordLength': 2,
+            'wordWeight': 0.8,
+        } ,space='mylingua', data_type=nmslib.DataType.OBJECT_AS_STRING, dtype=nmslib.DistType.FLOAT)
+        
+        X = [
+            ' '.join(random.choice(['1','0']) for _ in range(4*32)) for _ in range(6)
+        ]
+
+        index.addDataPointBatch(X)
+        index.createIndex({'post': 2}, print_progress=False)
+
+        for i in range(len(X)):
+            ids, dists = index.knnQuery(X[i], k=5)
+            
+            for id, dist in zip(ids, dists):
+                self.assertAlmostEqual(index.getDistanceToObj(X[id], X[i]), dist, places=6)
 
 
 
